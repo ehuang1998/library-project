9776c9eb79cbeb19afd408475a4547e9456857c9
Eric <16390263+ehuang1998@users.noreply.github.com>
2023-08-02 23:36:09 -0400

    Modal update

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+var modal = document.getElementById("modal");
+var addBookBtn = document.getElementById("add-book");
+var btnClose = document.getElementsByClassName("btn-close")[0];
+
+addBookBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+btnClose.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
 let myLibrary = [];
 
 // Book Object Constructor
@@ -27,3 +46,5 @@ function displayBooks() {
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295 , 'not read yet')
 
 console.log(theHobbit.info());
+
+
